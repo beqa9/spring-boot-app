c6653ceb1a8f331da7892e898be5aef11c07db59
beqa <beqabeqa329@gmail.com>
2025-09-11 16:07:51 +0400

    upload_project

---
+++ src/main/java/ge/softlab/spring_boot_app/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package ge.softlab.spring_boot_app;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("User Management API")
+                        .description("This API manages company.")
+                        .version("1.0.0"));
+    }
+}
